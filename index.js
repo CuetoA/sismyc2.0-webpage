a87aa1fa357d466380170196e2be5198e43129fd
Luis Rodriguez <lalo_luis_6@hotmail.com>
2021-08-25 19:50:54 -0500

    Nuevas funciones, se elimino la necesidad de los archivos registrar.html y visualizar.html, se añade más interacción a la página

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log('Hola Mundo')
+const secciones =           document.getElementsByClassName("seccion")
+const botonVisualizar =     document.getElementById("visualizar")
+const botonRegistrar =      document.getElementById("registrar")
+const botonArbol =          document.getElementById("boton-arbol")
+const botonAnillo =         document.getElementById("boton-anillo")
+const enlaceVisualizar =    document.getElementById("e-visualizar")
+const enlaceRegistrar =     document.getElementById("e-registrar")
+let verORegistrar =         document.getElementById('accion')
+
+mostrarSolo(secciones, 1)
+
+botonVisualizar.addEventListener('click', () => {
+    verORegistrar.innerHTML = "visualizar"
+    menu("visualizar")
+})
+enlaceVisualizar.addEventListener('click', () => {
+    verORegistrar.innerHTML = "visualizar"
+    menu("visualizar")
+})
+botonRegistrar.addEventListener('click', () => {
+    verORegistrar.innerHTML = "registrar"
+    menu("registrar")
+})
+enlaceRegistrar.addEventListener('click', () => {
+    verORegistrar.innerHTML = "registrar"
+    menu("registrar")
+})
+// Menu principal
+function menu(seleccion) {
+    mostrarSolo(secciones, 2)
+    botonArbol.addEventListener('click', () =>{
+        if (verORegistrar.innerHTML === "registrar") {
+            window.location.href = './Registrar/registrar-arbol.html'
+        }
+        else if (verORegistrar.innerHTML === "visualizar") {
+            window.location.href = './Visualizar/visualizar-arbol.html'
+        }
+    })
+    botonAnillo.addEventListener('click', () =>{
+        if (verORegistrar.innerHTML === "registrar") {
+            window.location.href = './Registrar/registrar-anillo.html'
+        }
+        else if (verORegistrar.innerHTML === "visualizar") {
+            window.location.href = './Visualizar/visualizar-anillo.html'
+        }
+    })
+
+}
+
+// Función para esconder todas las secciones y mostrar sólo una
+function mostrarSolo(vectorEl, index) {
+    let arrayEl = Array.prototype.filter.call(vectorEl, elem => {
+        return elem.nodeName === 'SECTION'
+    })
+    arrayEl.forEach( element => {element.style.display = 'none'});
+    arrayEl[index-1].style.display = 'block'
+}
+
+
