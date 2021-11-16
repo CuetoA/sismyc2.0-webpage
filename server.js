331f4bd80e24d8834dc07a058d588eb720a57b21
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-11-15 22:53:07 -0600

    Added serial declaration into server and socket event enviarSerial

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-// Constantes apra inicial servidor
+// Constantes para inicial servidor
 const http = require('http')
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-
-
 // Instalando servidor
 app.use(express.static(__dirname + '/')); // Main path
 server.listen(8080, () => {
 	console.log('Server listening on http://localhost:8080')
 });
+
+// Constantes para abrir el puerto serial
+const Serialport = require("serialport");
+const Readline = Serialport.parsers.Readline;
+const port = new Serialport('COM3', { baudRate: 9600, databits: 8, parity: 'none', stopbits: 1, flowControl: false, buffersize: 32768 });
+const parser = port.pipe(new Readline());
+
+
 
 
 // socket está escuchando
@@ -26,6 +32,8 @@ io.on("connection", (socket) => {
 	socket.on('apagar', (valor) => {
 		
 	});
+
+	socket.on('enviarSerial', (mensaje) => port.write(mensaje)});
 });
 
 
