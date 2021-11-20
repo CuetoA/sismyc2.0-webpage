54a75304bf02530deee7f0a71ac56ff5259b29f8
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-11-20 13:13:37 -0600

    trying to fix socket io not defined

--- 3 Modelo/Comunicaciones S7/recepcionDatosS7.js
+++ 3 Modelo/Comunicaciones S7/recepcionDatosS7.js
@@ -1,11 +1,11 @@
 // Socket
-//const socket = io();
+const socket = io();
 
 //export {maquinaDeEstados}
 
 function maquinaDeEstados(tiempo){
 	mensaje = 'CMD0001 1,16,0,0,0';
-	//socket.emit('enviarSerial', mensaje );
+	socket.emit('enviarSerial', mensaje );
 	console.log('hola')
 	setTimeout(() => maquinaDeEstados(tiempo), tiempo);
 };
@@ -14,11 +14,20 @@ function maquinaDeEstados(tiempo){
 
 function recibiendoDatos(datos){
 
+	if (datos[0] != 0){
+		// Significa que los datos son nuevos
+		const cache = datos
 
+	} else {
+		// Significa que los datos son viejos
+
+	}
 
 }
 
-
+function limpiarDatos(){
+	// Deberemos enviar un mensaje, perio primero, arreglar socket
+}
 
 
 
