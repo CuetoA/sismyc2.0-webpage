b20ba5c49c486ea6c22586f038a72ffaf4ba5d78
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-12-02 17:54:05 -0600

    Added the correct tree schema

--- 3 Modelo/Esquemas.js
+++ 3 Modelo/Esquemas.js
@@ -5,61 +5,61 @@ const Schema = mongoose.Schema;
 
 const objetoSchema = new Schema({
 	identificacion : {type: String, required: true},
-    fechaDeRegistro : {type: String, required: true},
+    fechaDeRegistro : {type: String, required: false},
     responsableDeRegistro : {type: String, required: true},
-    objetoRelacionado : {type: String, required: true},
-    ubicacion : {type: String, required: true}
-}, {timestamps: true});
+    objetoRelacionado : {type: String, required: false},
+    ubicacion : {type: String, required: false}
+}, {timestamps: false});
 
 
 const datosTelemetriaSchema = new Schema({
-	fechayHora: {type: String, required: true},
-	temperatura: {type: String, required: true},
-	humedad: {type: String, required: true},
-	ph: {type: String, required: true}
+	fechayHora: {type: String, required: false},
+	temperatura: {type: String, required: false},
+	humedad: {type: String, required: false},
+	ph: {type: String, required: false}
 	nutrientes: {
-		temperatura: {type: String, required: true},
-		humedad: {type: String, required: true},
-		nutrientes: {type: String, required: true}
+		temperatura: {type: String, required: false},
+		humedad: {type: String, required: false},
+		nutrientes: {type: String, required: false}
 	}
 });
 
 const arbolSchema = new Schema({
-	objeto: {type: objetoSchema, required: true},
+	objeto: {type: objetoSchema, required: false},
 
 	datosDeRiego: {
-		horaDeRiego: {type: String, required: true},
-		ciclosDeRiego: {type: String, required: true},
-		horaDeMedición: {type: String, required: true},
-		ciclosDeMedición: {type: String, required: true},
-		cantidadDeAgua por riego: {type: String, required: true},
-		cantidadDeFertilizante por riego: {type: String, required: true},
-		rangoDeTemperaturas: {type: String, required: true},
-		humedadMínima: {type: String, required: true}
+		horaDeRiego: {type: String, required: false},
+		ciclosDeRiego: {type: String, required: false},
+		horaDeMedición: {type: String, required: false},
+		ciclosDeMedición: {type: String, required: false},
+		cantidadDeAgua por riego: {type: String, required: false},
+		cantidadDeFertilizante por riego: {type: String, required: false},
+		rangoDeTemperaturas: {type: String, required: false},
+		humedadMínima: {type: String, required: false}
 	}
 
     datosDeRegistro: {
-    	identificaciónDelArbol: {type: String, required: true},
-		fechaDeRegistro: {type: String, required: true},
-		edadDeIngreso al sistema: {type: String, required: true},
-		responsableDelRegistro: {type: String, required: true},
+    	identificaciónDelArbol: {type: String, required: false},
+		fechaDeRegistro: {type: String, required: false},
+		edadDeIngreso al sistema: {type: String, required: false},
+		responsableDelRegistro: {type: String, required: false},
 		tamañoDeIngresoAlSistema: {
-			alturaDeIngreso: {type: String, required: true},
-			diámetroDeIngreso: {type: String, required: true}
+			alturaDeIngreso: {type: String, required: false},
+			diámetroDeIngreso: {type: String, required: false}
 		}
     }
 
     informacionDelArbol:{
-    	relacionConAnillo: {type: String, required: true},
-		ubicacion: {type: String, required: true}
+    	relacionConAnillo: {type: String, required: false},
+		ubicacion: {type: String, required: false}
 		taxonomia: {
-			familia: {type: String, required: true},
-			Género: {type: String, required: true},
-			Especie: {type: String, required: true}
+			familia: {type: String, required: false},
+			Género: {type: String, required: false},
+			Especie: {type: String, required: false}
 		}
     }
 
-    datosDeTelemetria: {type: [datosTelemetriaSchema], required: true}
+    datosDeTelemetria: {type: [datosTelemetriaSchema], required: false}
 });
 
 /*
