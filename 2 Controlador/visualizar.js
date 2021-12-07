c884f4d66b1e500975428a5ea3660c11435ea471
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-12-06 22:43:24 -0600

    Nailed to select the id through dropdown

--- 2 Controlador/visualizar.js
+++ 2 Controlador/visualizar.js
@@ -4,9 +4,9 @@ const socket = io();
 var dropdownArbol = document.getElementById("dropdownArbol");
 var dropdownMenu = document.getElementById("dropdown-menu");
 
-
+mostrarListadoArboles()
 // Eventos de botones
-dropdownArbol.onclick = () => {mostrarListadoArboles()};
+dropdownArbol.onclick = () => {};
 
 function mostrarListadoArboles(){
 	solicitarListaArboles();	
@@ -31,16 +31,24 @@ function desplegarElementos(lista){
 	console.log('elemento 2: ', lista[0]._id)
 	console.log('elemento 3: ', lista[2])
 	*/
-	console.log(Object.keys(dropdownMenu))
+	//console.log(Object.keys(dropdownMenu))
 	
 	for (let elemento in lista){
 		
 		var opt = lista[elemento];
-		var el = document.createElement("dropdown-item")
+		var el = document.createElement("button")
+		el.className = 'dropdown-item'
 		el.textContent = lista[elemento].datosDeRegistro.id + "\n\n";
 		el.value = lista[elemento]._id;
+
+		el.setAttribute('onclick', `enviarDatos('${lista[elemento]._id}')`)
 		dropdownMenu.appendChild(el)
 
 		//console.log('elementos: ', lista)
 	}
+}
+
+function enviarDatos(llegada){
+	console.log('Hola bb chompi hermoso')
+	console.log(llegada)
 }
