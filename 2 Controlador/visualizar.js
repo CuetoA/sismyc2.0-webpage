65a90c98904e93aa5115ef316c6d42417a7327df
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-12-07 09:06:31 -0600

    unpacked data from Json file

--- 2 Controlador/visualizar.js
+++ 2 Controlador/visualizar.js
@@ -55,5 +55,102 @@ function enviarDatos(elemento){
 }
 
 socket.on('datosSolicitados', (datos) =>{
-	console.log(datos);
+	//console.log('tomamesta' , datos);
+	colocarDatos(datos);
 });
+
+
+function colocarDatos(datos){
+	//console.log(datos[0])
+	//console.log(datos[0].datosDeRegistro.edadDeIngreso)
+	
+}
+/*
+
+{
+    "datosDeRiego": {
+        "rangoTemperaturaInferior": "",
+        "rangoTemperaturaSuperior": "",
+        "rangoHumedadInferior": "",
+        "rangoHumedadSuperior": "",
+        "ciclosMedicionNumero": "",
+        "ciclosMedicionUnidad": null,
+        "fertilizantePorRiego": "",
+        "aguaPorRiego": "",
+        "ciclosDeRiego": "",
+        "ciclosDeRiegoUnidad": null
+    },
+    "datosDeRegistro": {
+        "tamañoDeIngresoAlSistema": {
+            "alturaDeRegistro": "500",
+            "diametroDeRegistro": "2"
+        },
+        "id": "Tainna Three",
+        "fechaDeRegistro": "06/12/2021",
+        "edadDeIngreso": "20",
+        "registrante": "Tainna Santana"
+    },
+    "informacionDelArbol": {
+        "taxonomia": {
+            "familia": "0",
+            "genero": "0",
+            "especie": "0"
+        },
+        "anilloRelacionado": "20",
+        "ubicacion": "Masagua 5"
+    },
+    "_id": "61ae60c65aae543dbb8f5930",
+    "datosDeTelemetria": [
+        {
+            "nutrientes": {
+                "n": "20",
+                "p": "20",
+                "k": "20"
+            },
+            "fechayHora": "06/12/21 01:15",
+            "temperatura": "30",
+            "humedad": "91",
+            "ph": "7",
+            "_id": "61ae6156a2a17e0e109c89b5"
+        },
+        {
+            "nutrientes": {
+                "n": "20",
+                "p": "20",
+                "k": "20"
+            },
+            "fechayHora": "06/12/21 01:15",
+            "temperatura": "30",
+            "humedad": "91",
+            "ph": "7",
+            "_id": "61ae77703a578cad3b857043"
+        },
+        {
+            "nutrientes": {
+                "n": "20",
+                "p": "20",
+                "k": "20"
+            },
+            "fechayHora": "06/12/21 01:15",
+            "temperatura": "30",
+            "humedad": "91",
+            "ph": "7",
+            "_id": "61ae78604296ca526335204c"
+        },
+        {
+            "nutrientes": {
+                "n": "20",
+                "p": "20",
+                "k": "20"
+            },
+            "fechayHora": "06/12/21 01:15",
+            "temperatura": "30",
+            "humedad": "91",
+            "ph": "7",
+            "_id": "61aea8ef2bea11c176e65ccc"
+        }
+    ],
+    "__v": 0
+
+    */
+}
